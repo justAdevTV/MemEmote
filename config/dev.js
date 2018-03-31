af11f9251cbb4e302ac998fb76beab7c90508f6e
Justin <justpunz@live.com>
2018-03-31 02:23:50 -0700

    Added userdata with google 0auth

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // TODO: CHANGE THESE
-    googleClientID: process.env.FACEBOOK_CLIENT_ID,
-    googleClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: 'IBiUnwUdh5LvsEki1AZyD5MaU4gQ16tT'
-};
