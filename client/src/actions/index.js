099891b030ca081d9c6775951cbfb8a0c2a48d00
Brian Canedo <bcanedo4@gmail.com>
2018-03-31 05:03:00 -0700

    Wired up actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { 
+    FETCH_USER,
+    FETCH_CARDS,
+    FETCH_CARD,
+    CREATE_CARD,
+    DELETE_CARD
+} from './types';
+
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
@@ -9,3 +16,40 @@ export const fetchUser = () => async dispatch => {
         payload: res.data
     });
 };
+
+export function fetchCards() {
+    const request = axios.get('/api/cards');
+    return {
+        type: FETCH_CARDS,
+        payload: request
+    };
+}
+
+export function createCard(values, callback) {
+    const request = axios.post('/api/cards', values)
+    .then(() => callback());
+
+    return {
+        type: CREATE_CARD,
+        payload: request
+    };
+}
+
+export function fetchCard(id) {
+    const request = axios.get(`/api/card/${id}`);
+
+    return {
+        type: FETCH_CARD,
+        payload: request
+    };
+}
+
+export function deleteCard(id, callback) {
+    const request = axios.delete(`/api/card/${id}`)
+        .then(() => callback());
+
+    return {
+        type: DELETE_CARD,
+        payload: id
+    };
+}
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,5 @@
 export const FETCH_USER = 'fetch_user';
+export const FETCH_CARDS = 'fetch_cards';
+export const FETCH_CARD = 'fetch_card';
+export const CREATE_CARD = 'create_cards';
+export const DELETE_CARD = 'delete_card';
