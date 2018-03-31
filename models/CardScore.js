29a7df0d8707d29e52bf1ebc4b72b4a54a1be507
Justin <justpunz@live.com>
2018-03-31 07:38:03 -0700

    Updated Score for cards

--- models/CardScore.js
+++ models/CardScore.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const cardScoreSchema = new Schema({
-    joy: Number,
-    anger: Number,
-    sorrow: Number,
-    suprise: Number
+    joy: {default: 0, type: Number},
+    anger: {default: 0, type: Number},
+    sorrow: {default: 0, type: Number},
+    suprise: {default: 0, type: Number}
 });
 
 module.exports = cardScoreSchema;
