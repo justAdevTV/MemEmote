ef31d77a966ba206d81ac5abd3986a4955cd231f
Justin <justpunz@live.com>
2018-03-31 04:18:37 -0700

    Added Webcam with picture taking

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
+import WebcamCapture from './WebcamCapture';
 
 class Landing extends Component {
     render() {
         return (
             <div>
                 <h1>Landing Page from React</h1>
+                <WebcamCapture />
             </div>
         );
     }
