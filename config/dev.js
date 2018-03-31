6c809893e191e6b08e95f61f39f4a458dcf389f4
Justin <justpunz@live.com>
2018-03-31 01:24:15 -0700

    Added default client page to server and added cookies, keys, and passpt

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // TODO: CHANGE THESE
+    googleClientID: process.env.FACEBOOK_CLIENT_ID,
+    googleClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: 'IBiUnwUdh5LvsEki1AZyD5MaU4gQ16tT'
+};
