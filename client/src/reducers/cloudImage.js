4118436e07b890e2e12e65b8afa5636ce125f221
Justin <justpunz@live.com>
2018-04-01 10:27:52 -0700

    Added temp delay to face

--- client/src/reducers/cloudImage.js
+++ client/src/reducers/cloudImage.js
@@ -3,6 +3,9 @@ import {SEND_EMOTION_PICTURE} from '../actions/types';
 export default function(state = null, action) {
     switch (action.type) {
         case SEND_EMOTION_PICTURE:
+            setTimeout(function() { //Start the timer
+                window.location.reload(); 
+            }.bind(this), 7000);
             return action.payload || false;
         default:
             return state;
